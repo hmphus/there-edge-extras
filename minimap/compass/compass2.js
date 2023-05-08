79b05d8b77e855519af4634261dbd40433f26181
Brian Gontowski <brian@gontowski.com>
2023-05-08 23:33:36 +0900

    Added track selection

--- minimap/compass/compass2.js
+++ minimap/compass/compass2.js
@@ -2,7 +2,12 @@ There.init({
   data: {
     location: {},
     places: {},
+    tracks: {},
     zoom: 10,
+    windows: {
+      version: 0,
+      names: {},
+    },
   },
 
   onReady: function() {
@@ -10,29 +15,51 @@ There.init({
       width: 200,
       height: 200,
     });
-
-    There.fsCommand('setWidthHeight', {
-      width: 200,
-      height: 200,
-    });
-
     There.fsCommand('setTextureBitDepth', {
       depth: 32,
     });
+    new ResizeObserver(function(entries) {
+      const rect = entries[0].contentRect;
+      There.fsCommand('setWidthHeight', {
+        width: rect.width,
+        height: rect.height,
+      });
+    }).observe($('.compass')[0]);
   },
 
   onVariable: function(name, value) {
     if (name == 'there_teleporting' || name == 'there_ready') {
       $('.compass').attr(name.replace('there_', 'data-'), value);
     }
-
     if (name == 'there_avheading') {
-      $('.compass').css('--heading', `${value}deg`);
+      $('.compass .main').css('--heading', `${value}deg`);
     }
-
     if (name == 'there_ready' && value == 1) {
+      There.fetchPilotXml(),
       There.fetchLocationXml();
       There.fetchPlaces();
+    }
+    if (name == 'there_configurationchanged' && value == 1) {
+      There.setNamedTimer('pilot', 1000, There.fetchPilotXml);
+    }
+  },
+
+  fetchPilotXml: async function() {
+    await There.fetchAsync({
+      path: '/ClientLoginGui/pilotInfo',
+      dataType: 'xml',
+      success: There.onPilotXml,
+    });
+  },
+
+  onPilotXml: function(xml) {
+    const xmlAnswer = xml.getElementsByTagName('Answer')[0];
+    const doid = Number(xmlAnswer.getElementsByTagName('PilotDoid')[0].childNodes[0].nodeValue);
+    const name = xmlAnswer.getElementsByTagName('PilotName')[0].childNodes[0].nodeValue;
+    if (doid != 0 && name != '') {
+      There.variables.there_pilotdoid = doid;
+      There.variables.there_pilotname = name;
+      $('.compass').attr('data-ready', '1');
     }
   },
 
@@ -84,7 +111,7 @@ There.init({
         $('.compass .altimeter span').text(text);
       },
       onOffset: function(offset) {
-        $('.compass').css('--offset-x', `${offset.x}px`).css('--offset-y', `${offset.y}px`)
+        $('.compass .main').css('--offset-x', `${offset.x}px`).css('--offset-y', `${offset.y}px`)
       },
       onTile: function(tile) {
         for (let x in [0, 1, 2]) {
@@ -195,19 +222,280 @@ There.init({
     values = values.map(v => v * zoom);
     return (tile.x >= values[0]) && (tile.x < values[0] + values[1]) && (tile.y >= values[2]) && (tile.y < values[2] + values[3]);
   },
+
+  fetchTracks: function() {
+    if ($('.compass .pick').attr('data-loading') == 1) {
+      return;
+    }
+    $('.compass .pick').attr('data-loading', '1');
+    $.ajax({
+      url: 'https://www.hmph.us/there/api/minimap/tracks/',
+      method: 'POST',
+      data: {
+        avatar_name: There.variables.there_pilotname ?? '',
+      },
+      dataType: 'json',
+      success: function(data) {
+        for (let entry of data.tracks) {
+          entry.menus = [{
+            text: 'Go',
+            action: 'go',
+            enabled: entry.teleport ? true : false,
+          }, {
+            text: 'Join',
+            action: 'join',
+            enabled: true,
+          }, {
+            text: 'About',
+            action: 'about',
+            enabled: true,
+          }];
+        }
+        There.data.tracks['*'] = data.tracks ?? [];
+        There.setupPick();
+      },
+      complete: function() {
+        $('.compass .pick').attr('data-loading', '0');
+      },
+    });
+  },
+
+  setupPick: function() {
+    let divItems = $('.compass .pick .items');
+    $(divItems).find('.item').remove();
+    for (let entry of There.data.tracks['*']) {
+      There.setupPickEntry(divItems, entry);
+    }
+  },
+
+  setupPickEntry: function(divItems, entry) {
+    let divItem = $('<div class="item"></div>');
+    let divName = $('<div class="name"></div>');
+    $(divItem).attr('data-id', entry.id);
+    $(divName).text(entry.name);
+    if (There.data.menu?.id == entry.id) {
+      $(divItem).attr('data-hover', '1');
+    }
+    $(divItem).data('entry', entry);
+    $(divItem).append($(divName)).appendTo($(divItems));
+    $(divItem).on('mouseover', function() {
+      const timeout = $('.contextmenu').attr('data-active') == 1 ? 500 : 350;
+      There.setNamedTimer('context-menu', timeout, function() {
+        if (There.data.menu?.id != entry.id) {
+          There.setupContextMenu(divItem, entry);
+        }
+      });
+    }).on('mousemove', function(event) {
+      event.stopPropagation();
+    }).on('dblclick', function() {
+      There.playSound('menu item activate');
+      There.clearContextMenu();
+      // TODO: Load track
+    });
+  },
+
+  clearContextMenu: function() {
+    There.clearNamedTimer('context-menu');
+    $('.contextmenu').attr('data-active', '0');
+    $('.compass .pick .items .item').attr('data-hover', '0');
+    There.data.menu = {};
+  },
+
+  setupContextMenu: function(divItem, entry) {
+    let divContextMenu = $('.contextmenu');
+    $(divContextMenu).find('.item').remove();
+    $('.compass .pick .items .item').attr('data-hover', '0');
+    for (let index in entry.menus) {
+      const menu = entry.menus[index];
+      let divMenuItem = $('<div class="item"></div>');
+      let divSound = $('<div class="sound"></div>');
+      $(divMenuItem).attr('data-index', index);
+      $(divMenuItem).attr('data-enabled', menu.enabled ? '1' : '0');
+      $(divSound).text(menu.text);
+      $(divMenuItem).append($(divSound)).appendTo($(divContextMenu));
+      if (menu.enabled) {
+        $(divSound).on('mouseover', function() {
+          There.playSound('enabled menu item rollover');
+        });
+        $(divMenuItem).on('mousedown', function(event) {
+          event.stopPropagation();
+        }).on('click', function() {
+          There.playSound('menu item activate');
+          There.clearContextMenu();
+          if (menu.action == 'go') {
+            let url = `https://webapps.prod.there.com/goto/goto?obj=${entry.teleport}`;
+            There.fetchClientWindowsXml(function() {
+              There.fsCommand('browser', url);
+              setTimeout(function() {
+                There.fetchClientWindowsXml(function() {
+                  for (let name of Object.keys(There.data.windows.names).filter(k => There.data.windows.names[k] == There.data.windows.version)) {
+                    There.fetchClientWindowCloseButtonXml(name, function() {
+                      There.fetch({
+                        path: '/ScriptHook/Invoke',
+                        query: {
+                          Path: `/client/windows/${name}/close`,
+                        },
+                        dataType: 'xml',
+                      });
+                    });
+                  }
+                });
+              }, 2000);
+            });
+
+          }
+          if (menu.action == 'join') {
+          }
+          if (menu.action == 'about') {
+            There.fsCommand('browser', entry.url);
+          }
+        });
+      } else {
+        $(divSound).on('mouseover', function() {
+          There.playSound('disabled menu item rollover');
+        });
+      }
+    }
+    const maxY = $('.compass').height() - $(divContextMenu).height() - 4;
+    const top = $(divItem).offset().top;
+    const y = Math.min(top + 4, maxY);
+    $(divContextMenu).css('top', y).attr('data-active', '1');
+    $(divItem).attr('data-hover', '1');
+    There.data.menu = {
+      id: entry.id,
+      top: top,
+    };
+  },
+
+  fetchClientWindowsXml: function(callback) {
+    There.data.ident = Math.random();
+    There.fetch({
+      path: '/ScriptHook/Get',
+      query: {
+        Path: '/client/windows',
+      },
+      dataType: 'xml',
+      success: There.onClientWindowsXml,
+      complete: callback,
+    });
+  },
+
+  onClientWindowsXml: function(xml) {
+    const xmlAnswer = xml.getElementsByTagName('Answer')[0];
+    const xmlResult = xmlAnswer.getElementsByTagName('Result')[0];
+    if (xmlResult.childNodes[0].nodeValue != 1) {
+      return;
+    }
+    const xmlVersion = xmlAnswer.getElementsByTagName('Version')[0];
+    There.data.windows.version = xmlVersion.childNodes[0].nodeValue;
+    const xmlNode = xmlAnswer.getElementsByTagName('Node')[0];
+    const xmlChildren = xmlNode.getElementsByTagName('Children')[0];
+    for (let xmlChild of xmlChildren.childNodes) {
+      if (xmlChild.nodeName == 'Child') {
+        const name = xmlChild.getElementsByTagName('Name')[0].childNodes[0].nodeValue;
+        if (!There.data.windows.names.hasOwnProperty(name)) {
+          There.data.windows.names[name] = There.data.windows.version;
+        }
+      }
+    }
+  },
+
+  fetchClientWindowCloseButtonXml: function(name, callback) {
+    There.data.ident = Math.random();
+    There.fetch({
+      path: '/ScriptHook/Get',
+      query: {
+        Path: `/client/windows/${name}/hasCloseButton`,
+      },
+      dataType: 'xml',
+      success: function(xml) {
+        if (There.onClientWindowCloseButtonXml(xml)) {
+          callback();
+        }
+      },
+    });
+  },
+
+  onClientWindowCloseButtonXml: function(xml) {
+    const xmlAnswer = xml.getElementsByTagName('Answer')[0];
+    const xmlResult = xmlAnswer.getElementsByTagName('Result')[0];
+    if (xmlResult.childNodes[0].nodeValue != 1) {
+      return false;
+    }
+    const xmlNode = xmlAnswer.getElementsByTagName('Node')[0];
+    const xmlValue = xmlNode.getElementsByTagName('Value')[0];
+    if (xmlValue.childNodes[0].nodeValue != 1) {
+      return false;
+    }
+    return true;
+  },
 });
 
 $(document).ready(function() {
-  $('.compass').on('mousedown', function(event) {
+  $('.compass .main').on('mousedown', function(event) {
     if (event.which == 1) {
       There.fsCommand('beginDragWindow');
     }
+    There.clearContextMenu();
     event.preventDefault();
     event.stopPropagation();
   });
 
-  $('.compass .button[data-id="close"]').on('click', function() {
-    There.fsCommand('closeWindow');
+  $('.compass .button').on('mousedown', function() {
+    There.clearContextMenu();
+  });
+
+  $('.contextmenu').on('mousemove', function(event) {
+    There.clearNamedTimer('context-menu');
+    event.stopPropagation();
+  });
+
+  $('.compass').on('mouseleave', function(event) {
+    There.setNamedTimer('context-menu', 500, function() {
+      There.clearContextMenu();
+    });
+  }).on('mousemove', function() {
+    There.setNamedTimer('context-menu', 350, function() {
+      There.clearContextMenu();
+    });
+  });
+
+  $('.compass .pick .items').on('scroll', function() {
+    There.clearContextMenu();
+  });
+
+  $('.compass .button[data-id="close"]').on('click', function(event) {
+    if ($(this).attr('data-enabled') == 0) {
+      return;
+    }
+    if (event.shiftKey) {
+      There.fsCommand('openDevTools');
+    } else {
+      There.fsCommand('closeWindow');
+    }
+  }).on('mouseover', function(event) {
+    There.playSound('control rollover');
+  }).on('mousedown', function(event) {
+    There.playSound('control down');
+    event.stopPropagation();
+  }).on('mouseup', function(event) {
+    There.playSound('control up');
+  });
+
+  $('.compass .button[data-id="expand"]').on('click', function(event) {
+    if (event.shiftKey) {
+      There.fsCommand('newChildPluginWindow', {
+        id: 'There_gpsd',
+        url: `http://${There.variables.there_resourceshost}/Resources/Compass/gpsd.swf`,
+      });
+    } else {
+      if ($('.compass').attr('data-mode') == 'map') {
+        $('.compass').attr('data-mode', 'pick');
+        There.fetchTracks();
+      } else {
+        $('.compass').attr('data-mode', 'map');
+      }
+    }
   }).on('mouseover', function(event) {
     There.playSound('control rollover');
   }).on('mousedown', function(event) {
